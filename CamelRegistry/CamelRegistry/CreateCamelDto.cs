1bb2c56abd231350a284fb56441d200f557d966d
Szabolcs Németh <69214707+nemethszabii@users.noreply.github.com>
2026-02-05 13:23:48 +0100

    Add validation attributes to CreateCamelDto properties

--- CamelRegistry/CamelRegistry/CreateCamelDto.cs
+++ CamelRegistry/CamelRegistry/CreateCamelDto.cs
@@ -5,9 +5,11 @@ namespace CamelRegistry
 	public class CreateCamelDto
 	{
 		[Required]
+		[MinLength(3)]
 		[MaxLength(100)]
 		public string Name { get; set; }
 		[Required]
+		[MinLength(3)]
 		[MaxLength(50)]
 		public string Color { get; set; }
 		public int HumpCount { get; set; }
